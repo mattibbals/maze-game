cd3a34c8b24d671535fea9d49645a4870bb5299f
Matthew Win Tibbals <mattibbals@yahoo.com>
2023-06-08 19:14:11 -0500

    adding_build_dir

---
+++ build/precache-manifest.35a6a9ecff51c8f1979dd9374e7e3f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0470b16e29523af5cc2f0db0587298e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05421a623af85072ff8d",
+    "url": "/static/css/main.9d1d5abe.chunk.css"
+  },
+  {
+    "revision": "5411301b51034a355374",
+    "url": "/static/js/2.6699b6ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6699b6ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05421a623af85072ff8d",
+    "url": "/static/js/main.c9d05b42.chunk.js"
+  },
+  {
+    "revision": "61b7fe7547ce1eedccc5",
+    "url": "/static/js/runtime-main.824cd311.js"
+  }
+]);
